7e0a5ddd4b0bc890e087e4a9d07f605cc06950f7
YOUR NAME <cecilia@bartlewski.de>
2024-09-09 11:52:02 +0200

    changed code accordingly

--- js-objects-and-arrays_objects/index.js
+++ js-objects-and-arrays_objects/index.js
@@ -4,8 +4,11 @@ console.clear();
 // Create an object personWithNameAgeEmail with the properties "name", "age" and "email".
 
 const personWithNameAgeEmail = {
-  example: "example",
+  name: "cece",
+  age: "30",
+  email: "thisisnot@my-email.com",
 };
+console.log(personWithNameAgeEmail);
 
 // EXERCISE 2
 // Set the values of name and age equal to the corresponding values of the personAlex object.
@@ -15,8 +18,10 @@ const personAlex = {
   age: 24,
 };
 
-const nameOfAlex = "Change me";
-const ageOfAlex = "Change me";
+const nameOfAlex = "name";
+const ageOfAlex = "24";
+
+console.log(personAlex);
 
 // EXERCISE 3
 // Change the value of name to "Alex" and the value of age to "35" by updating the object personToChange: person.existingProperty = newValue.
@@ -25,6 +30,8 @@ const personToChange = {
   name: "Kim",
   age: 24,
 };
+(personToChange.name = "Alex"), (personToChange.age = "35");
+console.log(personToChange);
 
 // EXERCISE 4
 // Add the properties age "5" and breed "husky" to the object petPluto: pet.newProperty = newValue.
@@ -33,6 +40,10 @@ const petPluto = {
   name: "Pluto",
   species: "dog",
 };
+petPluto.age = "5";
+petPluto.breed = "husky";
+
+console.log(petPluto);
 
 export {
   personWithNameAgeEmail,
