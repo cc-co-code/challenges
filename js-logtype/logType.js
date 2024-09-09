4752b4bf2d3541d4f9d259da09995a9771a15660
YOUR NAME <cecilia@bartlewski.de>
2024-09-09 10:33:31 +0200

    logtype challenge

---
+++ js-logtype/logType.js
@@ -0,0 +1,63 @@
+let data;
+
+switch (typeof data) {
+  case "undefined":
+    console.log("undefined!");
+    break;
+}
+
+case "number";
+if (isNaN(data)){
+    console.log("not a number!")
+} else {
+    console.log("number!")
+}
+break;
+
+/* nachfragen was das bedeutet
+case "string":
+    console.log("string")
+    break;
+
+    case "boolean":
+*/
+
+/* Solution code
+    break;
+    case "string":
+        console.log("this is a string");
+        break;
+    case "boolean":
+        console.log("this is a boolean");
+        break;
+    case "bigint":
+        console.log("this is a bigint");
+        break;
+    case "function":
+        console.log("this is a function");
+        break;
+    case "undefined":
+        console.log("this is a function"); warum ist "undefind" auch "funktion?"
+        break;
+    default:
+        console.log("no idea what this is");
+}
+        */
+
+const "answer";
+switch (answer) {
+    case summer:
+        console.log("it is summer!")
+}
+
+  
+
+break;
+
+case spring:
+    console.log("it is spring!")
+
+    break;
+
+    default
+    console.log("this is not a valid anser!")
