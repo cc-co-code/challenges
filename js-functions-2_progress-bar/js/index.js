5e635827792680c0cdc1e1da4017a0cd621ccb6e
YOUR NAME <cecilia@bartlewski.de>
2024-09-10 13:47:18 +0200

    added comments for my personal better understanding and to remember what i did

--- js-functions-2_progress-bar/js/index.js
+++ js-functions-2_progress-bar/js/index.js
@@ -19,18 +19,32 @@ For that:
  (HINT: you can change the width of an element like this: element.style.width = '10px')
 */
 
+// Select the progress bar element using its data attribute
 const progressBar = document.querySelector('[data-js="progress-bar"]');
 
+// Function to calculate how much the user has scrolled in percentage
 function calculateScrollPercentage() {
+  // Get the current scroll position (how far the user has scrolled)
   const scrollTop = window.scrollY;
+
+  // Get the height of the visible window (the part of the page the user can see)
   const windowHeight = window.innerHeight;
+
+  // Get the total height of the webpage (including the parts the user can't see without scrolling)
   const documentHeight = document.body.clientHeight;
 
+  // Calculate the scroll percentage: current scroll divided by total scrollable height
   const scrollPercentage = (scrollTop / (documentHeight - windowHeight)) * 100;
+
+  // Return the scroll percentage value
   return scrollPercentage;
 }
 
+// Add an event listener to trigger when the user scrolls
 document.addEventListener("scroll", () => {
+  // Calculate the current scroll percentage when scrolling
   const scrollPercent = calculateScrollPercentage();
+
+  // Set the width of the progress bar to match the scroll percentage
   progressBar.style.width = `${scrollPercent}%`;
 });
