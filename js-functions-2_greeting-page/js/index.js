145aa2e1a6b50d8bc82918abe8b4f63a7ccc9cfe
YOUR NAME <cecilia@bartlewski.de>
2024-09-10 11:16:52 +0200

    solved challenge with return and if structure

--- js-functions-2_greeting-page/js/index.js
+++ js-functions-2_greeting-page/js/index.js
@@ -23,10 +23,33 @@ Change the contents of this page depending on the current day and time.
 const display = document.querySelector('[data-js="display"]');
 
 function getGreeting() {
-  // Code here
+  const hour = new Date().getHours();
+  if (hour >= 6 && hour <= 12) {
+    return "Good morning";
+  }
+  if (hour >= 13 && hour <= 18) {
+    return "Good Afternoon";
+  }
+  if (hour >= 19 && hour <= 22) {
+    return "Good Evening";
+    // Code here
+  }
+  if (hour >= 22 && hour <= 5) {
+    return "Good Night";
+  }
 }
 
 function getDayColor() {
+  const weekday = new Date().getDay();
+  if (weekday === 1) {
+    return "darkgray";
+  }
+  if (weekday <= 2 && weekday <= 5) {
+    return "lightblue";
+  }
+  if (weekday <= 6 && weekday <= 0) {
+    return "hotpink";
+  }
   // Code here
 }
 
