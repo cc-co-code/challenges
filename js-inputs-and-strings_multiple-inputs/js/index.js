9b3950c47715f867b5159ae5ca217c2743de4899
YOUR NAME <cecilia@bartlewski.de>
2024-09-11 13:56:25 +0200

    added eventlistener

--- js-inputs-and-strings_multiple-inputs/js/index.js
+++ js-inputs-and-strings_multiple-inputs/js/index.js
@@ -14,3 +14,13 @@ Implement the following functionality:
 const firstInput = document.querySelector("[data-js=first-input]");
 const secondInput = document.querySelector("[data-js=second-input]");
 const button = document.querySelector("[data-js=button]");
+
+button.addEventListener("click" () => {
+
+    
+}
+
+
+
+
+)
