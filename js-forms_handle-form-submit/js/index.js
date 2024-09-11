e9eff899092d969cfc23c524ccfd4074f7034fbd
YOUR NAME <cecilia@bartlewski.de>
2024-09-11 16:15:27 +0200

    finished handle-form-submit challenge

--- js-forms_handle-form-submit/js/index.js
+++ js-forms_handle-form-submit/js/index.js
@@ -1,3 +1,24 @@
 console.clear();
 
 const form = document.querySelector('[data-js="form"]');
+const firstNameInput = document.querySelector('[data-js="first-name"]');
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const formData = new FormData(event.target);
+  const data = Object.fromEntries(formData);
+
+  console.log(data);
+
+  const age = parseInt(data.age, 10);
+  const badness = parseInt(data.badness, 10);
+  const ageBadnessSum = age + badness;
+
+  console.log(
+    `The age-badness-sum of "${data.firstName}" is "${ageBadnessSum}"`
+  );
+
+  form.reset();
+  firstNameInput.focus();
+});
