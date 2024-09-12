f0c592c81db4b6cb92757e38aaea4cea7d66960f
YOUR NAME <cecilia@bartlewski.de>
2024-09-12 11:17:04 +0200

    finished toast challenge

--- js-createelement_toast-messages/js/index.js
+++ js-createelement_toast-messages/js/index.js
@@ -4,10 +4,17 @@ const toastContainer = document.querySelector('[data-js="toast-container"]');
 const addButton = document.querySelector('[data-js="add-button"]');
 const clearButton = document.querySelector('[data-js="clear-button"]');
 
-addButton.addEventListener("click", () => {
+addButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  const newToast = document.createElement("li");
+  newToast.classList.add("toast-container__message");
+  newToast.textContent = "new toast message";
+
+  toastContainer.append(newToast);
   // Exercise: Append a new entry to the toast messages container
 });
 
 clearButton.addEventListener("click", () => {
+  toastContainer.innerHTML = "";
   // Exercise: Clear the stack of toast messages
 });
