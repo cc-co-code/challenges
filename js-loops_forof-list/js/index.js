fd9a388cb3f64308fba474c7a569dc03dbd4150e
YOUR NAME <cecilia@bartlewski.de>
2024-09-16 15:04:06 +0200

    Added for..of to list the ordered items

--- js-loops_forof-list/js/index.js
+++ js-loops_forof-list/js/index.js
@@ -1,5 +1,23 @@
 console.clear();
 
+/* # For...of to create a list
+
+We can use loops to iterate through an array and do some stuff with each item. Let's create HTML elements with the help of `for...of` and a given array!
+
+## Task
+
+Looking at the `./js/index.js` , there is
+
+- an `ol` created and appended to the document
+- a string array stored to a variable called `programmingLanguages`.
+
+For each array item, create a list item for the ordered list with its value as visible text. Use the `for...of` loop.
+
+The following hints may guide you:
+
+- What was the property to set the text of an HTML element again?
+- Don't forget to append each list item! */
+
 const main = document.querySelector('[data-js="main"]');
 
 // Create a list from an array
@@ -18,5 +36,7 @@ const programmingLanguages = [
 ];
 
 // --v-- write/change code here --v--
-
+for (const programmingLanguage of programmingLanguages) {
+  console.log(programmingLanguage);
+}
 // --^-- write/change code here --^--
