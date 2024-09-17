c10b7f06004c3cd7bf7c7dcef6e7b431d288c225
YOUR NAME <cecilia@bartlewski.de>
2024-09-17 12:13:57 +0200

    finished challenge

--- js-callback-functions_user-login-callback-4-with-multiple-functions/js/index.js
+++ js-callback-functions_user-login-callback-4-with-multiple-functions/js/index.js
@@ -1,5 +1,9 @@
 console.clear();
-
+/*1.  Write a function with the "function" keyword named "showWelcomeMessage".
+    The function should accept two parameters: "userName" and "userRole".
+    The function should log the following message to the console:
+    "Welcome ${userName}! You are logged in now as ${userRole}."
+    */
 function handleUserLogin(onSuccess, onError, userName) {
   if (userName === "Jane Doe") {
     onSuccess(userName, "admin");
@@ -8,4 +12,33 @@ function handleUserLogin(onSuccess, onError, userName) {
   }
 }
 
-// The exercise starts here!
+function showWelcomeMessage(userName, userRole) {
+  console.log(`Welcome ${userName}! You are logged in now as ${userRole}.`);
+}
+
+/*Write a function with the "function" keyword named "showErrorMessage".
+    The function should accept one parameter "errorMessage"
+    The function should log the following message to the console:
+    "Login error: ${errorMessage}"*/
+
+function showErrorMessage(errorMessage) {
+  console.log(`Login error: ${errorMessage}`);
+}
+
+/*Call the function "handleUserLogin" and pass three arguments in this order:
+
+- callback function: "showWelcomeMessage"
+- callback function: "showErrorMessage"
+- user name string: "Jane Doe"
+  (You should see the log from "showWelcomeMessage" in the console)*/
+
+handleUserLogin(showWelcomeMessage, showErrorMessage, "Jane Doe");
+
+/*Call the function "handleUserLogin" and pass three arguments in this order:
+
+- callback function: "showWelcomeMessage"
+- callback function: "showErrorMessage"
+- user name string: "John Doe"
+  (You should see the log from "showErrorMessage" in the console)*/
+
+handleUserLogin(showWelcomeMessage, showErrorMessage, "John Doe");
