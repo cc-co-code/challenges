eb1f831de174b12c8e1570e8f4f7568be6285b82
YOUR NAME <cecilia@bartlewski.de>
2024-09-19 10:59:04 +0200

    did not finish this, i cant find the error

---
+++ js-structure_quiz-app/components/state/cards.js
@@ -0,0 +1,30 @@
+// state/cards.js
+
+// Define the cards array (global state)
+export default function {
+   const cards = [
+        {
+          question:
+            "In the Kingdom Hearts series who provides the English voice for Master Eraqus?",
+          answer: "Mark Hamill",
+          tags: ["kingdomheart", "mastereraqus", "english voices"],
+          isBookmarked: false,
+        },
+        {
+          question:
+            "Daniel Radcliffe became a global star in the film industry due to his performance in which film franchise?",
+          answer: "Harry Potter",
+          tags: ["hewhomustnotbenamed", "danielradcliffe", "abracadabra"],
+          isBookmarked: false,
+        },
+        {
+          question:
+            'In the game series "The Legend of Zelda" what is the name of the main character?',
+          answer: "Link",
+          tags: ["linktothepast", "linkbetweenworlds", "linkbetweenages"],
+          isBookmarked: false,
+        },
+      ];
+      return cards;
+} 
+// This is exported so that it can be imported in other files
