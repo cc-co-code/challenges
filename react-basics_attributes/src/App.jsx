d02f205fbb2a6684e82e3a0e94eb46fcc64226ad
YOUR NAME <cecilia@bartlewski.de>
2024-10-01 15:06:59 +0200

    finished 3. react basics challenge

--- react-basics_attributes/src/App.jsx
+++ react-basics_attributes/src/App.jsx
@@ -1,5 +1,22 @@
 import "./App.css";
 
 export default function App() {
-  return <div>Replace me with an article</div>;
+  return <Article />;
+}
+
+export function Article() {
+  return (
+    <article className="article">
+      <h2 className="article__title">Hows the weather on mars</h2>
+      <p>Didnt you always want to know the weather on mars</p>
+      <label htmlFor="weather">Enter your guess</label>
+      <input type="text" id="weather"></input>
+      <a
+        className="article__link"
+        href="https://en.wikipedia.org/wiki/Climate_of_Mars"
+      >
+        here you can find out how the weather really is
+      </a>
+    </article>
+  );
 }
