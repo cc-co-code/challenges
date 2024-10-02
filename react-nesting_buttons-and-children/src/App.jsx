dad0c10d9c070d7fb6b8e6397a71c780d11ba11e
YOUR NAME <cecilia@bartlewski.de>
2024-10-02 12:10:07 +0200

    finisehd 1. nesting chall

--- react-nesting_buttons-and-children/src/App.jsx
+++ react-nesting_buttons-and-children/src/App.jsx
@@ -3,18 +3,26 @@ import "./App.css";
 export default function App() {
   return (
     <main>
-      <Button />
-      <Button />
-      <Button />
-      <Button />
+      <Button>Hi</Button>
+      <Button>How are you</Button>
+      <Button>Good Morning</Button>
+      <Button>Bye</Button>
     </main>
   );
 }
 
-function Button() {
+// eslint-disable-next-line react/prop-types
+function Button({ children }) {
   return (
     <button className="button" type="button">
-      Click me!
+      {children}
     </button>
   );
 }
+
+/*1. in the `Button` component, receive the `children` prop as parameter; make sure to destructure it.
+2. replace the "Click me!" text with the `children` prop (don't forget the curly braces `{}`).
+3. in the `App` component, change the `Button` elements from being self-closing to have an 
+opening and closing tag.
+4. add text of your choice between the opening and closing tags of the Button elements 
+(use different texts to see any difference in the browser!)*/
