f7b59a92e82bfc29f45a03b2fbc2bf1f84b1b1be
YOUR NAME <cecilia@bartlewski.de>
2024-10-14 15:17:51 +0200

    finished challenge from friday

--- lotr-app/pages/index.js
+++ lotr-app/pages/index.js
@@ -1,7 +1,11 @@
+import Link from "next/link";
+
 export default function HomePage() {
   return (
     <div>
-      <h1>Hello from Next.js</h1>
+      <h1>Lord of the Rings</h1>
+      <p>Explore all the volumes of The Lord of the Rings:</p>
+      <Link href="/volumes">Go to Volumes</Link>
     </div>
   );
 }
