38ecbe473112f5fb3d5382194a87ef2c236f7432
YOUR NAME <cecilia@bartlewski.de>
2024-10-29 10:43:48 +0100

    challenge is not correct, couldnt fix error

--- backend-read_products/pages/api/products/index.js
+++ backend-read_products/pages/api/products/index.js
@@ -1,5 +1,13 @@
-import { products } from "../../../lib/products";
+import dbConnect from "@/db/model/connect";
+import Product from "@/components/Product";
 
-export default function handler(request, response) {
-  return response.status(200).json(products);
+export default async function handler(request, response) {
+  await dbConnect();
+
+  if (request.method === "GET") {
+    const Product = await Product.find();
+    return response.status(200).json(Product);
+  } else {
+    return response.status(405).json({ message: "Method not allowed" });
+  }
 }
